68aefb887023d747e01345ef8fc5b8e090181774
shrey924 <144999403+shrey924@users.noreply.github.com>
2026-06-18 11:47:22 +0530

    ss
    s

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import os
 
 st.set_page_config(
     page_title="Credit Card Fraud Detection",
@@ -9,7 +10,10 @@ st.set_page_config(
 )
 
 # Load Model
-model = joblib.load("credit_card_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "credit_card_model.pkl")
+
+model = joblib.load(MODEL_PATH)
 
 st.title("💳 Credit Card Fraud Detection")
 
@@ -17,8 +21,7 @@ st.markdown("""
 Upload a CSV file containing transaction data.
 
 Required columns:
-
-Time, V1 ... V28, Amount
+Time, V1, V2, ... , V28, Amount
 """)
 
 uploaded_file = st.file_uploader(
@@ -31,22 +34,38 @@ if uploaded_file is not None:
     try:
         df = pd.read_csv(uploaded_file)
 
+        # Remove target column if present
+        if "Class" in df.columns:
+            df = df.drop("Class", axis=1)
+
+        expected_columns = [
+            'Time','V1','V2','V3','V4','V5','V6','V7','V8','V9',
+            'V10','V11','V12','V13','V14','V15','V16','V17',
+            'V18','V19','V20','V21','V22','V23','V24','V25',
+            'V26','V27','V28','Amount'
+        ]
+
+        # Ensure correct column order
+        df = df[expected_columns]
+
         st.subheader("Uploaded Data")
         st.dataframe(df.head())
 
         prediction = model.predict(df)
 
-        df["Prediction"] = prediction
+        result_df = df.copy()
 
-        df["Prediction"] = df["Prediction"].map({
+        result_df["Prediction"] = prediction
+
+        result_df["Prediction"] = result_df["Prediction"].map({
             0: "Legitimate",
             1: "Fraud"
         })
 
         st.subheader("Prediction Result")
-        st.dataframe(df.head())
+        st.dataframe(result_df.head())
 
-        csv = df.to_csv(index=False)
+        csv = result_df.to_csv(index=False)
 
         st.download_button(
             label="Download Results",
